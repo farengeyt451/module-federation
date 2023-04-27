9bfc6fa05df1c7ea93c04e5719407528169ba88a
Oleksandr Kyslov <icoleksandr.kyslov@amwell.com>
2023-04-27 16:44:13 +0300

    Communication Between Micro Frontends and Sharing Monorepo Libraries

--- projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
+++ projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
@@ -1,11 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AuthLibService } from 'auth-lib';
 
 @Component({
   selector: 'app-flights-search',
   templateUrl: './flights-search.component.html',
   styleUrls: ['./flights-search.component.css']
 })
-export class FlightsSearchComponent {
+export class FlightsSearchComponent implements OnInit {
+  public user: string | null = null;
+
+  constructor(private service: AuthLibService) { }
+
+  ngOnInit(): void {
+    this.user  = this.service.user;
+  }
 
   search(): void {
     alert('Not implemented for this demo!');
